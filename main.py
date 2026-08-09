337bf71665c5fee7450f11a4fd15de505933bb64
cauafurtado77 <cauaalves.rs@gmail.com>
2026-08-09 15:03:40 -0300

    função cadastrar e sair do programa

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
-def cadastro():
-    
-    
+# lista dos livros
+livros = []
+
+
+# função para cadastrar um livro
+def cadastrar_livro(livros):
+
+    print("\n  CADASTRAR LIVRO ")
+
+    titulo = input("Digite o título: ")
+    autor = input("Digite o autor: ")
+    ano = input("Digite o ano: ")
+    isbn = input("Digite o ISBN: ")
+
+    livro = {
+        "titulo": titulo,
+        "autor": autor,
+        "ano": ano,
+        "isbn": isbn,
+        "status": "disponível"
+    }
+
+    livros.append(livro)
+    print("Livro cadastrado.")
+    return livros
+
+
+# aqui é feito o menu da biblioteca
+while True:
+
+    print("\n BIBLIOTECA  ")
+    print("1 - Cadastrar livro")
+    print("2 - Emprestar livro")
+    print("3 - Devolver livro")
+    print("4 - Listar livros")
+    print("5 - Buscar livro")
+    print("6 - Ordenar livros")
+    print("7 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+
+        livros = cadastrar_livro(livros)
+
+    elif opcao == "7":
+
+        print("Programa encerrado ")
+        break
+
+    else:
+
+        print("Essa opção ainda não existe")
